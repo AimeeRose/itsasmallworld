eef94caacd8646ff205233752da118626108c6ec
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-03-22 00:22:54 -0400

    Initial commit

--- app/app.js
+++ app/app.js
@@ -9,4 +9,152 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]).
+directive('myCurrentTime', ['$interval', 'dateFilter', function($interval, dateFilter) {
+  function link(scope, element, attrs) {
+    var format,
+        timeoutId;
+    function updateTime() {
+      element.text(dateFilter(new Date(), format));
+    }
+
+    scope.$watch(attrs.myCurrentTime, function(value) {
+      format = value;
+      updateTime();
+    });
+
+    element.on('$destroy', function() {
+      $interval.cancel(timeoutId);
+    });
+    // start the UI update process; save the timeoutId for canceling
+    timeoutId = $interval(function() {
+      updateTime(); // update DOM
+    }, 1000);
+  }
+  return {
+    link: link
+  };
+}]).
+directive('azimuthalMap', [function() {
+  function drawMap() {
+    var width = 1280;
+    var height = 800;
+    var centered;
+    var cityAirportData;
+    var feature;
+
+    var projection = d3.geo.azimuthal()
+        .scale(380)
+        .origin([-71.03,42.37])
+        .mode("orthographic")
+        .translate([640, 400]);
+
+    var circle = d3.geo.greatCircle()
+        .origin(projection.origin());
+
+    // TODO fix d3.geo.azimuthal to be consistent with scale
+    var scale = {
+      orthographic: 380,
+      stereographic: 380,
+      gnomonic: 380,
+      equidistant: 380 / Math.PI * 2,
+      equalarea: 380 / Math.SQRT2
+    };
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    var svg = d3.select(".map").append("svg:svg")
+        .attr("width", width)
+        .attr("height", height)
+        .on("mousedown", mousedown);
+
+    // Groups
+    var geoGroup = svg.append("g").attr("id", "geo-paths")
+
+    // Draw the world
+    d3.json("data/world-countries.json", function(collection) {
+      feature = geoGroup.selectAll("path").data(collection.features).enter().append("svg:path")
+        .attr("d", clip)
+        .on("click", clicked);
+
+      feature.append("svg:title")
+          .text(function(d) { return d.properties.name; });
+    });
+
+    d3.select(window)
+        .on("mousemove", mousemove)
+        .on("mouseup", mouseup);
+
+    d3.select("select").on("change", function() {
+      projection.mode(this.value).scale(scale[this.value]);
+      refresh(750);
+    });
+
+    // MOVE THE WORLD AROUND
+
+    var m0,
+        o0;
+
+    function mousedown() {
+      m0 = [d3.event.pageX, d3.event.pageY];
+      o0 = projection.origin();
+      d3.event.preventDefault();
+    }
+
+    function mousemove() {
+      if (m0) {
+        var m1 = [d3.event.pageX, d3.event.pageY],
+            o1 = [o0[0] + (m0[0] - m1[0]) / 8, o0[1] + (m1[1] - m0[1]) / 8];
+        projection.origin(o1);
+        circle.origin(o1)
+        refresh();
+      }
+    }
+
+    function mouseup() {
+      if (m0) {
+        mousemove();
+        m0 = null;
+      }
+    }
+
+    function refresh(duration) {
+      (duration ? feature.transition().duration(duration) : feature).attr("d", clip);
+    }
+
+    function clip(d) {
+      return path(circle.clip(d));
+    }
+
+    function clicked(d) {
+      var x, y, k;
+
+      if (d && centered !== d) {
+        var centroid = path.centroid(d);
+        x = centroid[0];
+        y = centroid[1];
+        k = 4;
+        centered = d;
+      } else {
+        x = width / 2;
+        y = height / 2;
+        k = 1;
+        centered = null;
+      }
+      geoGroup.selectAll("path")
+          .classed("active", centered && function(d) { return d === centered; });
+
+      // FIXME: DRY ME
+      geoGroup.transition()
+          .duration(750)
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+          .style("stroke-width", 1.5 / k + "px");
+    }
+  }
+
+  return {
+    //templateUrl: 'viz/azimuthalMap.html'
+    link: drawMap
+  };
 }]);

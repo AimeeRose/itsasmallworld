f9eba267c1a4a6ed5028d81b949547316ce26812
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-04-05 18:59:22 -0400

    Working on big cities populations

--- app/chartCities.js
+++ app/chartCities.js
@@ -1,20 +1,61 @@
+var populationPoints = [];
 function chartCities(rootScope) {
-  var svg = d3.select(".charts").append("svg:svg")
-      .attr("width", 1000)
-      .attr("height", 1200)
-  rootScope.svg = svg
+  var tick = undefined;
 
-  var populationsGroup = svg.append('g').attr('id', 'cityPopulations')
-  var cx = 0
+  var width = $('.main').innerWidth() - 120, // padding + 40
+      height = 1200;
+
+  var pointGrid = d3.layout.grid()
+    .points()
+    .size([width, 500]);
+
+  var svg = d3.select(".charts").append("svg")
+    .attr({
+      width: width,
+      height: height
+    }).append("g").attr("transform", "translate(70,70)");
+
+  var cityTooltip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) { return d.City + ', ' + d.Country });
+  svg.call(cityTooltip);
+
+  function update() {
+    var point = svg.selectAll(".point")
+      .data(pointGrid(populationPoints));
+    point.enter().append("circle")
+      .attr("fill", "skyblue")
+      .attr("class", "point")
+      .attr("r", 1e-6)
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+      .on("mouseover", cityTooltip.show)
+      .on("mouseout", cityTooltip.hide);
+    point.transition()
+      .attr("r", function(d) { return d.pop2015 / 1000});
+  }
+
+  function push() {
+    //populationPoints.push({});
+    update();
+    if (populationPoints.length > 15) {
+      clearInterval(tick);
+      tick = setInterval(pop, 500);
+    }
+  }
+
+  function pop() {
+    populationPoints.pop();
+    update();
+    if (populationPoints.length < 2) {
+      clearInterval(tick);
+      tick = setInterval(push, 500);
+    }
+  }
+
+  //var populationsGroup = svg.append('g').attr('id', 'cityPopulations')
   d3.tsv("data/urbanareas1_1.tsv", function(data) {
-    // select 5 cities
-    // make a grid
-
-    populationsGroup.selectAll('circle').data(data).enter()
-      .append('circle')
-      .attr('r', function(d) { return d.pop2015 / 100})
-      .attr('fill', 'yellow')
-      .attr('cx', function(d) { cx += d.pop2015 / 100; return cx })
-      .attr('cy', 100)
+    populationPoints = _.select(data, function(city) { return city.pop2015 > 3000 });
+    update();
   });
 }

1d53f78eae0d84f47d1f9565baba4de069456bbb
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-04-12 18:44:15 -0400

    Densities

--- app/chartCities.js
+++ app/chartCities.js
@@ -1,13 +1,9 @@
 function chartCities(rootScope) {
-  var populationPoints = [];
+  // TODO: Add each row by ticks
   var tick = undefined;
 
   var width = $('.main').innerWidth() - 120, // padding + 40
-      height = 1200;
-
-  var pointGrid = d3.layout.grid()
-    .points()
-    .size([width, 500]);
+      height = 5000;
 
   var svg = d3.select(".charts").append("svg")
     .attr({
@@ -15,85 +11,46 @@ function chartCities(rootScope) {
       height: height
     }).append("g").attr("transform", "translate(70,70)");
 
-  var cityTooltip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-10, 0])
-    .html(function(d) { return d.City + ', ' + d.Country });
-  svg.call(cityTooltip);
+  var rowHeight = 100;
+  var rowWidth = 0;
+  var rowItemMargin = 10;
+  var rowMargin = 50;
 
-  function updatePopulations() {
-    var point = svg.selectAll(".point")
-      .data(pointGrid(populationPoints));
-    point.enter().append("circle")
-      .attr("fill", "skyblue")
-      .attr("class", "point")
-      .attr("r", 1e-6)
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-      .on("mouseover", cityTooltip.show)
-      .on("mouseout", cityTooltip.hide);
-    point.transition()
-      .attr("r", function(d) { return d.pop2015 / 1000});
-  }
+  var createPopulationDensityGrid = function(city, yheight) {
+    // A mini grid for representing density
+    var popDensityGrid = d3.layout.grid()
+      .bands()
+      .size([rowHeight,rowHeight])
+      .padding([0.1, 0.1]);
 
-  //var populationsGroup = svg.append('g').attr('id', 'cityPopulations')
-  d3.tsv("data/urbanareas1_1.tsv", function(data) {
-    populationPoints = _.select(data, function(city) { return city.pop2015 > 3000 });
-    updatePopulations();
-  });
+    // Create a group for the little squares
+    // TODO: Add mouseover for density
+    var densityGroup = svg.append('g').attr('id', city.UrbanArea + '-density')
+      .attr("transform", function(d) { return "translate(" + 0 + "," + yheight + ")"; })
+    
+    // An array of ints representing the density. The more ints the greater the density.
+    var density = _.map(_.range(city.Density / 100), function(v) { return {'idx': v} })
 
-  var populationDensities = [];
-  // var rectGrid = d3.layout.grid()
-  //   .bands()
-  //   .size([width, 500])
-  //   .padding([0.1, 0.1]);
-
-  var densitiesGroup = svg.append('g').attr('id', 'densitiesPopulation')
-    .attr("transform", function(d) { return "translate(" + 0 + "," + 540 + ")"; })
-
-  function updateDensities() {
-    var rect = densitiesGroup.selectAll(".rect")
-      .data(rectGrid(populationDensities));
+    // Fill in the group with the density
+    var rect = densityGroup.selectAll(".rect")
+      .data(popDensityGrid(density));
     rect.enter().append("rect")
       .attr("class", "rect")
-      .attr("width", rectGrid.nodeSize()[0])
-      .attr("height", rectGrid.nodeSize()[1])
-      .attr("transform", function(d) { return "translate(" + 0+ "," + d.y + ")"; })
-      .style("opacity", 1e-6);
-    rect.transition()
-      .attr("width", rectGrid.nodeSize()[0])
-      .attr("height", rectGrid.nodeSize()[1])
+      .attr("width", popDensityGrid.nodeSize()[0])
+      .attr("height", popDensityGrid.nodeSize()[1])
       .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
       .style("opacity", 1);
   }
 
   d3.csv("data/demographia-populations.csv", function(data) {
     populationDensities = _.select(data, function(city) { return city.Population > 3000000 });
-    //updateDensities();
 
-    var rectGrid = d3.layout.grid()
-      .bands()
-      .size([100,100])
-      .padding([0.1, 0.1]);
+    var yheight = 0;
 
     var transform = [0,0]
     _.each(populationDensities, function(city) {
-      // create a group
-      var densityGroup = svg.append('g').attr('id', city.UrbanArea)
-        .attr("transform", function(d) { return "translate(" + transform[0] + "," + transform[1] + ")"; })
-      transform[0] += 100; transform[1] += 100
-
-      var density = _.map(_.range(city.Density / 100), function(v) { return {'idx': v} })
-
-      // fill in the group with the density
-      var rect = densityGroup.selectAll(".rect")
-        .data(rectGrid(density));
-      rect.enter().append("rect")
-        .attr("class", "rect")
-        .attr("width", rectGrid.nodeSize()[0])
-        .attr("height", rectGrid.nodeSize()[1])
-        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-        .style("opacity", 1);
-
+      createPopulationDensityGrid(city, yheight);
+      yheight += rowHeight + rowMargin;
     })
   });
 }
